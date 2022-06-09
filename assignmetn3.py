eb1d08662ad52d409b76eae4a61a7a213a08db2c
VanshSingh95075 <104381790+VanshSingh95075@users.noreply.github.com>
2022-06-09 22:56:55 +0530

    Update assignmetn3.py

--- assignmetn3.py
+++ assignmetn3.py
@@ -15,7 +15,36 @@ for i in d:
 
 
 #QUESTION2
-# check if choice is one of the four options
+# Program make a simple calculator
+
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
